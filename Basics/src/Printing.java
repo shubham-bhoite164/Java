af35b16fbad40346acb6dbc64d5786b4b83c7d76
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-05-15 14:28:26 +0530

    Add files via upload

---
+++ Basics/src/Printing.java
@@ -0,0 +1,29 @@
+public class Printing {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println(3 + 3);
+
+        // This will print it on the same line
+        System.out.print("Hello World! ");
+        System.out.print("I will print on the same line.");
+
+        // To do multiline comment
+        /*System.out.print("Hello World! ");
+        System.out.print("I will print on the same line."); */
+
+        // To print anything on new line
+        System.out.println("\nShubham\nBhoite");
+
+        // \n\n to give more gaps vertically
+        System.out.print("\n\nGood morning\nAll set?\nLet's learn Java");
+        // It will print like:
+
+        //Good morning
+        //All set?
+        //Let's learn Java
+
+        // to give space like Tab
+        System.out.println("Shubham\tBhoite");
+    }
+}
