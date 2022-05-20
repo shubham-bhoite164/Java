a02a5dc63a72894547e3b0a530c66cd3752ac377
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-05-20 16:10:50 +0530

    Update Practise.java

--- Basics/src/Practise.java
+++ Basics/src/Practise.java
@@ -4,35 +4,35 @@ import java.util.Scanner;
 public class Practise {
     public static void main(String[] args) {
 
-        //Make a program that takes the radius of a circle as input,
-        // calculates its radius and area and prints it as output to the user.
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter the Radius");
-//        double radius = sc.nextInt();
-//        double output = Math.pow(radius,2);
-//        double area = 3.14*output;
-//
-//        System.out.println("The area of circle is: " );
-//        System.out.println(area);
+       // Make a program that takes the radius of a circle as input,
+       //calculates its radius and area and prints it as output to the user.
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the Radius");
+       double radius = sc.nextInt();
+       double output = Math.pow(radius,2);
+       double area = 3.14*output;
+
+       System.out.println("The area of circle is: " );
+       System.out.println(area);
 
 
 
         //Make a program that prints the table of a number that is input by the user.
         //Without Using Loops
-//        Scanner sc = new Scanner(System.in);
-//        System.out.println("Enter the Number for which you want to generate the table");
-//        int num = sc.nextInt();
-//        System.out.println("The Table is :");
-//        System.out.println(num*1);
-//        System.out.println(num*2);
-//        System.out.println(num*3);
-//        System.out.println(num*4);
-//        System.out.println(num*5);
-//        System.out.println(num*6);
-//        System.out.println(num*7);
-//        System.out.println(num*8);
-//        System.out.println(num*9);
-//        System.out.println(num*10);
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Enter the Number for which you want to generate the table");
+       int num = sc.nextInt();
+       System.out.println("The Table is :");
+       System.out.println(num*1);
+       System.out.println(num*2);
+       System.out.println(num*3);
+       System.out.println(num*4);
+       System.out.println(num*5);
+       System.out.println(num*6);
+       System.out.println(num*7);
+       System.out.println(num*8);
+       System.out.println(num*9);
+       System.out.println(num*10);
 
 
 
