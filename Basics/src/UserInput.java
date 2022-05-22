82411e39f60067f2abf1a6845aabb6308eded9d2
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-05-22 10:54:34 +0530

    Update UserInput.java

--- Basics/src/UserInput.java
+++ Basics/src/UserInput.java
@@ -12,19 +12,21 @@ public class UserInput {
 
         float c = sc.nextFloat();
         double d = sc.nextDouble();
-//        System.out.println("Enter number 1");
-//        int a = sc.nextInt();
-//        float a = sc.nextFloat();
-//        System.out.println("Enter number 2");
-//        int b = sc.nextInt();
-//        float b = sc.nextFloat();
+        
+        
+       System.out.println("Enter number 1");
+       int a = sc.nextInt();
+       float a = sc.nextFloat();
+       System.out.println("Enter number 2");
+       int b = sc.nextInt();
+       float b = sc.nextFloat();
 
-//        int sum = a +b;
-//        float sum = a +b;
-//        System.out.println("The sum of these numbers is");
-//        System.out.println(sum);
-//        boolean b1 = sc.hasNextInt();
-//        System.out.println(b1);
-//        String str = sc.next();
+       int sum = a +b;
+       float sum = a +b;
+       System.out.println("The sum of these numbers is");
+       System.out.println(sum);
+       boolean b1 = sc.hasNextInt();
+       System.out.println(b1);
+       String str = sc.next();
     }
 }
