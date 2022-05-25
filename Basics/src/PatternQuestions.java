6be481e0bd62c7bceba8d3c9c180b1298969a505
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-05-25 16:51:43 +0530

    Update PatternQuestions.java

--- Basics/src/PatternQuestions.java
+++ Basics/src/PatternQuestions.java
@@ -56,6 +56,43 @@ public class PatternQuestions {
              }
              System.out.println();
          }
+        
+        
+        
+        // Printing Reverse Pyramid
+//        ****
+//        ***
+//        **
+//        *
+
+       for (int i=4; i>=1; i--){
+
+           for (int j=1; j<=i; j++){
+
+               System.out.print("*");
+           }
+           System.out.println();
+       }
+
+
+
+        // Inverted & Rotated Half Pyramid
+//          *
+//         **
+//        ***
+//       ****
+        
+        for (int i=1; i<=4; i++){
+
+            for (int j=1; j<=4-i; j++){
+
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println( );
+        }
 
     }
 }
