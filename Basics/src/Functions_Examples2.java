70cd8ce79ca4119a4afe4e23b61cc1bb5095bfc9
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-06-06 09:39:08 +0530

    Update Functions_Examples2.java

--- Basics/src/Functions_Examples2.java
+++ Basics/src/Functions_Examples2.java
@@ -1,26 +1,28 @@
  import java.util.Scanner;
 
 public class Functions_Examples2 {
-//    // Multiplication of two numbers
+    // Multiplication of two numbers
 
-//    public static int multiplicaion(int a,int b){
-//      int  mul = a*b;
-//      return mul;
-//    }
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//
-//        System.out.println("Enter the Number 1");
-//        int a = sc.nextInt();
-//
-//        System.out.println("Enter the Number 2");
-//        int b = sc.nextInt();
-//
-//        int mul = multiplicaion(a,b);
-//        System.out.println(mul);
-//    }
+   public static int multiplicaion(int a,int b){
+     int  mul = a*b;
+     return mul;
+   }
+   public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+
+       System.out.println("Enter the Number 1");
+       int a = sc.nextInt();
+
+       System.out.println("Enter the Number 2");
+       int b = sc.nextInt();
+
+       int mul = multiplicaion(a,b);
+       System.out.println(mul);
+   }
 
 
+ 
+ // Substractions of Two Numbers
     public static int substraction(int a, int b){
         int sub = a-b;
         return sub;
