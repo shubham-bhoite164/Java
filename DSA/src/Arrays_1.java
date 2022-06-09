35a28af66081d2dc61d60695e46086caa60fc508
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-06-09 21:10:55 +0530

    Update Arrays_1.java

--- DSA/src/Arrays_1.java
+++ DSA/src/Arrays_1.java
@@ -2,12 +2,18 @@ import java.util.Scanner;
 
 public class Arrays_1 {
     public static void main(String[] args) {
+        
         // Now we have to make an array by taking the input from the user
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of subjects");
         int num = sc.nextInt();
-
         int[] marks = new int[num];
+        
+        // In this for loop we are adding the values in the Array
+        for (int i=0;i<num;i++){
+            marks[i] = sc.nextInt();
+        }
+        // In this we are giving Output
         for (int i = 0; i<num; i++){
             System.out.println(marks[i]);
         }
