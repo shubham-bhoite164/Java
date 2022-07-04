d94fe69cd4d83d210cd9c905d92a3850f840b7d1
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-07-04 15:16:03 +0530

    Add files via upload

---
+++ DSA/src/Strings/src/string_1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class string_1 {
+
+    public static void main(String[] args) {
+
+        // Declaring a String
+        String fullName = "Shubham Bhoite";
+        String sentence = "I am a boy";
+
+        // By taking a user input
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name = sc.nextLine();
+        System.out.println("Your name is :"+name);
+
+    }
+}
