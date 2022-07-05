0e284729cb59130059e11e9b5933d67d8aa2c080
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-07-05 12:46:11 +0530

    Add files via upload

---
+++ DSA/src/Strings/src/string_2_concatenation.java
@@ -0,0 +1,10 @@
+public class string_2_concatenation {
+    public static void main(String[] args){
+        // Functions in Java
+        // concatenation means joining two Strings
+        String firstName = "Shubham";
+        String lastName = "Bhoite";
+        System.out.println(firstName+" "+lastName);
+
+    }
+}
