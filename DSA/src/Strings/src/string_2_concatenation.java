fbbaa7f028a0f5b10d665b995f56ae049a190f81
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-07-06 14:26:39 +0530

    Update string_2_concatenation.java

--- DSA/src/Strings/src/string_2_concatenation.java
+++ DSA/src/Strings/src/string_2_concatenation.java
@@ -1,10 +1,12 @@
 public class string_2_concatenation {
     public static void main(String[] args){
+        
         // Functions in Java
         // concatenation means joining two Strings
         String firstName = "Shubham";
         String lastName = "Bhoite";
         System.out.println(firstName+" "+lastName);
+        
 
     }
 }
