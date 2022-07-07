eae15101955f0cae1df953191d7961e68547ea1a
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-07-07 20:05:45 +0530

    Update Functions_Adv_String.java

--- Basics/src/Functions_Adv_String.java
+++ Basics/src/Functions_Adv_String.java
@@ -1,11 +1,15 @@
 public class Functions_Adv_String {
     public static void main(String[] args) {
+        
         System.out.println(name());
+        
     }
+    
     static String name(){
         String firstName = "Shubham";
         String secondName = "Bhoite";
         String fullName = firstName+" "+secondName;
         return fullName;
+        
     }
 }
