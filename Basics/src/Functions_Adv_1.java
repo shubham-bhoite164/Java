50be16133bd9036f22f2bc5c5c4e2eb5c0444b2d
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-07-07 19:11:13 +0530

    Add files via upload

---
+++ Basics/src/Functions_Adv_1.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Functions_Adv_1 {
+    public static void main(String[] args) {
+        int ans = sum();
+        System.out.println(ans);
+    }
+
+    static int sum() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the num1");
+        int a = sc.nextInt();
+        System.out.println("ENter the num2");
+        int b = sc.nextInt();
+
+        int add = a+b;
+        return add;
+
+    }
+}
+
+
+
+
