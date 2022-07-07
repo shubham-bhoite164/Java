781404697cbb2007173c095f469173497153ea6a
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-07-07 20:04:54 +0530

    Add files via upload

---
+++ Basics/src/Functions_Adv_String.java
@@ -0,0 +1,11 @@
+public class Functions_Adv_String {
+    public static void main(String[] args) {
+        System.out.println(name());
+    }
+    static String name(){
+        String firstName = "Shubham";
+        String secondName = "Bhoite";
+        String fullName = firstName+" "+secondName;
+        return fullName;
+    }
+}
