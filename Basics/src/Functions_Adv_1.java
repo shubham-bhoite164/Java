d4f41dd47297b01f4e01d2daf0222aebbbd10230
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-07-07 19:12:00 +0530

    Update Functions_Adv_1.java

--- Basics/src/Functions_Adv_1.java
+++ Basics/src/Functions_Adv_1.java
@@ -2,11 +2,14 @@ import java.util.Scanner;
 
 public class Functions_Adv_1 {
     public static void main(String[] args) {
+        
         int ans = sum();
         System.out.println(ans);
+        
     }
 
     static int sum() {
+        
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the num1");
         int a = sc.nextInt();
