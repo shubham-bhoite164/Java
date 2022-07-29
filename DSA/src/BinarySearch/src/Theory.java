930fe1ea59e3af6645b08dad810403d5d22c36f6
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-07-29 16:45:45 +0530

    Update Theory.java

--- DSA/src/BinarySearch/src/Theory.java
+++ DSA/src/BinarySearch/src/Theory.java
@@ -1,5 +1,27 @@
 public class Theory {
     public static void main(String[] args) {
+        
+        // Works only for sorted array
 
+        // Step 1
+        // Find the middle element
+        // Middle element = (Start+End)/2
+
+        // Step 2
+        // Check weather the element we want is in right side of array or left side of an array.
+        // As array is sorted we can easily find it.
+        // if target element > middle element then search in right
+        // if target element < middle element then search in left
+
+        // Step 3
+        // If Middle element then it is the Ans.
+
+        // Step 4
+        // Again divide the array and keep on dividing it until we find the required element
+
+
+        // Total comparisons in worst case is log(N)
+        // Where N is the size of an array
+        
     }
 }
