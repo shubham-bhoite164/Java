7329861810417c2e0e376f72cb2adb2e6ee4042c
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-08-05 15:34:50 +0530

    Add files via upload

---
+++ DSA/src/BubbleSort/src/Theory.java
@@ -0,0 +1,8 @@
+public class Theory {
+    public static void main(String[] args) {
+        // In every step you are comparing the adjacent elements.
+        // If the next element is greater than current element then we are going to swap it.
+        // And keep on doing it.
+
+    }
+}
