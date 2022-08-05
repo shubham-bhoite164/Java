3bdffaf6b8236c757dc897371c4b363c48559a8e
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-08-05 22:32:48 +0530

    Update Theory.java

--- DSA/src/BubbleSort/src/Theory.java
+++ DSA/src/BubbleSort/src/Theory.java
@@ -1,8 +1,12 @@
 public class Theory {
     public static void main(String[] args) {
+        
         // In every step you are comparing the adjacent elements.
         // If the next element is greater than current element then we are going to swap it.
         // And keep on doing it.
+        
+        // Time Complexity is= ( Best case O(N) )  ( Worst case O(N) )
+        // space complexity is O(1), we don't need any extra space
 
     }
 }
