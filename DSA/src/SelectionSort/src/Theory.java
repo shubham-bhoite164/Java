bf98bdbf8f8d4fe2c12ef7c7de3ac60fc6f8e176
Shubham Bhoite <59025622+shubham-bhoite164@users.noreply.github.com>
2022-08-08 15:12:53 +0530

    Add files via upload

---
+++ DSA/src/SelectionSort/src/Theory.java
@@ -0,0 +1,12 @@
+public class Theory {
+    public static void main(String[] args) {
+
+        // Select a right element and put it at it's right position
+        // Here we put the largest element at last and keep and reducing the array.
+
+        // Same we can do using the maximum element
+
+        // It performs well in small lists
+
+    }
+}
